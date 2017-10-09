7a6b66149561250862a5d820c47dd9d437c833f0
cgomezb <cgomezb@psl.com.co>
2017-10-09 18:31:29 -0500

    css selector added

--- protractor/config.ts
+++ protractor/config.ts
@@ -3,9 +3,10 @@ import { browser, Config } from 'protractor';
 export const config: Config = {
   framework: 'jasmine',
   SELENIUM_PROMISE_MANAGER: false,
-  specs: ['../test/Google.spec.js'],
+  specs: ['../test/**/*.spec.js'],
   noGlobals: true,
   onPrepare: () => {
     browser.ignoreSynchronization = true;
-  }
+  },
+  getPageTimeout: 1000
 };
